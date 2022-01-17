409fe03fbe7908f9fec31a48ccfc01bc3711719d
Robert <roberttv17@gmail.com>
2022-01-17 13:01:58 -0600

    Correcciones

--- src/data/Coins.js
+++ src/data/Coins.js
@@ -1,6 +1,6 @@
 const coins = [
-    { id: 'USD', name: 'Dolar de Estados Unidos'},
-    { id: 'MXM', name: 'Peso Mexicado'},
+    { id: 'USD', name: 'Dólar de Estados Unidos'},
+    { id: 'MXM', name: 'Peso Mexicano'},
     { id: 'EUR', name: 'Euro'},
     { id: 'GBP', name: 'Libra Esterlina'},
 ];
